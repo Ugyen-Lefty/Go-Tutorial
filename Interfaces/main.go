1793c5e2813fa439a7e22a8f33b954173c2456ec
Lefty <ugyensamdrupdorji@gmail.com>
2023-08-23 19:12:29 +0600

    Http and interfaces

---
+++ Interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	//Combines all types that have getGreeting function that returns a string together in one type
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
